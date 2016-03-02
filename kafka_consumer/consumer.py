b0ce0ae48cf7a0b9a0845fdcdce654fae4d5e352
Eochs <slochastic@gmail.com>
2016-03-01 20:11:37 -0800

    more kafka stuff

--- kafka_consumer/consumer.py
+++ kafka_consumer/consumer.py
@@ -4,3 +4,13 @@ consumer = KafkaConsumer('my_favorite_topic')
 
 for msg in consumer:
   print (msg)
+
+
+from kafka import KafkaClient, SimpleConsumer
+from sys import argv
+kafka = KafkaClient("10.0.1.100:6667")
+consumer = SimpleConsumer(kafka, "my-group", argv[1])
+consumer.max_buffer_size=0
+consumer.seek(0,2)
+for message in consumer:
+  print("OFFSET: "+str(message[0])+"\t MSG: "+str(message[1][3]))
